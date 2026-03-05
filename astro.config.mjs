bd7c485208250ae0603f9a8bb115bbdd8cec5476
finahuel <nahuelitalo@gmail.com>
2026-03-04 21:24:05 -0300

    correccion astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
      site: 'https://finahuel.github.io',
-  base: '/Gambaastro/',
+  base: '/EntreHojas/',
   output: 'static',
   
     
